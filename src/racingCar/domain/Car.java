57c99363412ca68beedac271eda73e468c876f4b
saul <dlathf3210@naver.com>
2023-04-09 04:10:49 +0900

    Create Car.java

--- src/racingCar/domain/Car.java
+++ src/racingCar/domain/Car.java
@@ -3,5 +3,30 @@ package racingCar.domain;
 public class Car {
 
     private static final int GO_FORWARD_NUM = 4;
+    private static final String GO_FORWARD_MARKING = "_";
+    private static final int GO_FORWARD_DISTANCE = 1;
+
+    private final String name;
+    private int position;
+
+
+    private String displayCurrentPosition(){
+        StringBuilder currentPosition = new StringBuilder();
+        for(int i = 0; i < this.position; i++){
+            currentPosition.append(GO_FORWARD_MARKING);
+        }
+        return currentPosition.toString();
+    }
+
+
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 
 }
